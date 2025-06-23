5d0f063340f58c715d85c742a77689516bb3b106
zonghuang0924 <ludwig.albert.fbi@gmail.com>
2025-06-23 15:48:09 +0000

    Changes to be committed:

--- components/js.js
+++ components/js.js
@@ -6,3 +6,27 @@ document.addEventListener("mousemove", (e) => {
   cursor.style.left = e.pageX + "px";
   cursor.style.top = e.pageY + "px";
 });
+
+//滑鼠//
+function init() {
+
+  canvas = document.getElementById( 'world');
+    
+  if (canvas && canvas.getContext) {
+        context = canvas.getContext('2d');
+
+        // Register event listeners
+        window.addEventListener('mousemove', documentMouseMoveHandler, false);
+        window.addEventListener('mousedown', documentMouseDownHandler, false);
+        window.addEventListener('mouseup', documentMouseUpHandler, false);
+        document.addEventListener('touchstart', documentTouchStartHandler, false); 
+        document.addEventListener('touchmove', documentTouchMoveHandler, false);
+        window. addEventListener('resize', windowResizeHandler, false);
+
+        createParticles();
+
+        windowResizeHandler();
+
+        setInterval ( loop, 1000 / 60 );
+  }
+}
